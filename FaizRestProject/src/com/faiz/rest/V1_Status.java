359434c5579874a1a33d1d5baaf0fc071340f3d3
faizshaik16 <faizshaik16@gmail.com>
2015-04-09 22:36:59 -0400

    Initial Checkin Rest Project Faiz

---
+++ FaizRestProject/src/com/faiz/rest/V1_Status.java
@@ -0,0 +1,21 @@
+package com.faiz.rest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/status")
+public class V1_Status {
+
+	private static final String api_version ="1.0";
+	@GET
+	@Produces (MediaType.TEXT_HTML)
+	public String getMessage(){
+		return "<p> This is Faiz Rest Service Get Method </p>";
+	}
+	
+	@Path("/version")
+	@GET
+	@Produces (MediaType.TEXT_HTML)
+	public String secondaryPath(){
+		return " Version = " + api_version ;
+	}
+}
