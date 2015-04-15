704093d57e636e4c3aea8fab3c7b75aeb78cdf9e
faizshaik16 <faizshaik16@gmail.com>
2015-04-14 21:57:44 -0400

    Query Param Form Param Header Param

--- FaizRestProject/src/com/faiz/rest/V3_Param.java
+++ FaizRestProject/src/com/faiz/rest/V3_Param.java
@@ -23,9 +23,40 @@ public class V3_Param {
 	}
 	
 	@GET
-	@Path("pp/{userid}")
+	@Path("/pp/{userid}")
+	@Produces (MediaType.TEXT_HTML)
 	public String getUser(@PathParam("userid") String userId){
+		//http://localhost:8080/FaizRestProject/api/v3/status/pp/faiz
 		return "Path Parameter Userid = " + userId;
 	}
+	
+	@GET
+	@Path("/qp")
+	@Produces (MediaType.TEXT_HTML)
+	public String getQueryParam( @QueryParam("fname") String firstName){
+		
+		//http://localhost:8080/FaizRestProject/api/v3/status/qp?fname=faiz
+		return "Query Param FirstName =" + firstName;
+	}
+	
+	
+	@GET
+	@Path("/hp")
+	@Produces (MediaType.TEXT_HTML)
+	public String getHeaderParam( @HeaderParam("Host") String clientName){
+		
+		//http://localhost:8080/FaizRestProject/api/v3/status/hp
+		return "Header Param client name =" + clientName;
+	}
+	
+	@POST
+	@Path("/fp")
+	public String getFormParam( @FormParam("username") String username){
+		
+		System.out.println(" Form Parameter = " + username);
+		//http://localhost:8080/FaizRestProject/api/v3/status/hp
+		return "Form Param user name =" + username;
+	}
+
 
 }
